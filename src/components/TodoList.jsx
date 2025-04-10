3a354b166b4f2d4bfb6e41e533770ecb3a73a82b
Bhanu Pratap <bhanusahoo291100@gmail.com>
2025-04-11 00:13:54 +0530

    Fix Chakra all  issue

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 import TodoItem from "./TodoItem";
-import { VStack } from "@chakra-ui/react";
+import { VStack, Text } from "@chakra-ui/react";
 
 const TodoList = () => {
   const todos = useSelector((state) => state.todos.todos);
