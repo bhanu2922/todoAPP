f7d9df278b031a62b3c2a8f9e3e7ffb373429353
Bhanu Pratap <bhanusahoo291100@gmail.com>
2025-04-11 00:00:46 +0530

    Fix Chakra Text import issue

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,6 @@
 import { useDispatch } from "react-redux";
 import { deleteTodo, toggleTodo } from "../features/todos/TodoSlice.js";
-import { HStack, Text, Button, Checkbox } from "@chakra-ui/react";
+import { HStack, Button, Checkbox } from "@chakra-ui/react";
 
 const TodoItem = ({ todo }) => {
   const dispatch = useDispatch();
@@ -11,9 +11,7 @@ const TodoItem = ({ todo }) => {
         isChecked={TodoItem.compeleted}
         onChange={() => dispatch(toggleTodo(todo.id))}
       />
-      <p textDecoration={TodoItem.completed ? "line-through" : "none"}>
-        {todo.text}
-      </p>
+      <p>{todo.text}</p>
       <Button colorScheme="red" onClick={() => dispatch(deleteTodo(todo.id))}>
         Delete
       </Button>
